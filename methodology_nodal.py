58bf6c6e9cd298d9bab5cab8ba96d7cf87da880c
Martin Håberg <martin.haberg@statnett.no>
2019-01-16 10:57:25 +0100

    Updates made from SN

---
+++ methodology_nodal.py
@@ -0,0 +1,15 @@
+# Methodology script
+
+# Nodal model
+
+# Scenario specification
+# Fetch load Scenario
+# Fetch imbalance Scenario
+# Fetch balancing bids
+
+# Optimization
+# Find optimal activation based on nodal model (DCOPF)
+# Save cost results
+
+# Evaluation
+# Check if dispatch is feasible
---
+++ methodology_zonal_wo.py
@@ -0,0 +1,16 @@
+# Methodology script
+
+# Zonal model without filtering step
+
+# Scenario specification
+# Fetch load Scenario
+# Fetch imbalance Scenario
+# Fetch balancing bids
+# Generate ATCs based on load Scenario
+
+# Optimization
+# Find optimal activation based on zonal model (ATCs and zone imbalance)
+
+# Save cost results
+# Evaluation
+# Check if dispatch is feasible
